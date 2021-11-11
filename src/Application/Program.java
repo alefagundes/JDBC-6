3dd74301036a381c71108a49f974936607db0050
alefagundes <alefagundes2000.af@gmail.com>
2021-11-10 23:44:07 -0300

    Working transaction methods with jdbc connector

---
+++ src/Application/Program.java
@@ -0,0 +1,54 @@
+package Application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DBException;
+
+public class Program {
+
+	public static void main(String[] args){
+		Connection conn = null;
+		Statement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			conn.setAutoCommit(false);
+			
+			st = conn.createStatement();
+			
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 "
+					+ "WHERE DepartmentId = 1");
+			
+			/*int x = 1;
+			if(x<2) {
+				throw new SQLException("Fake Error!");
+			}*/
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 "
+					+ "WHERE DepartmentId = 2");
+			
+			conn.commit();
+			
+			System.out.println("Rows 1 :" + rows1);
+			System.out.println("Rows 2 :" + rows2);
+			
+		}catch(SQLException e) {
+			try {
+				conn.rollback();
+				throw new DBException("Transaction rolled back! Caused by: " + e.getMessage());
+				
+			} catch (SQLException e1) {
+				throw new DBException("Error trying to rollback! Cause by: " + e1.getMessage());
+			}
+		
+		}finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+	}
+}
